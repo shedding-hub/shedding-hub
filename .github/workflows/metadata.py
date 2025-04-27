f72c43e997e1be5b781e810beba198775df64b1c
Yuke Wang <yuke.wang@emory.edu>
2025-04-27 10:30:54 -0500

    using black to format metadata.py

--- .github/workflows/metadata.py
+++ .github/workflows/metadata.py
@@ -7,10 +7,8 @@ import glob
 from pathlib import Path
 
 
-def create_meta(
-    schema:dict
-) -> pd.DataFrame:
-    '''
+def create_meta(schema: dict) -> pd.DataFrame:
+    """
     Create data structure of metadata using schema file.
 
     Args:
@@ -18,22 +16,28 @@ def create_meta(
 
     Returns:
         Empty Metadata dataframe with column names.
-    '''
+    """
     # Initialize column names as a list with ID column
     column_names = ["ID"]
     # Add required fields of analyte into it;
     column_names = column_names + schema["$defs"]["analyte_specification"]["required"]
     # Add summary statistics into it;
-    column_names = column_names + ["n_samples", "n_unique_participants", "n_negative", "n_positive", "n_quantified"]
-    return pd.DataFrame(columns = column_names)
+    column_names = column_names + [
+        "n_samples",
+        "n_unique_participants",
+        "n_negative",
+        "n_positive",
+        "n_quantified",
+    ]
+    return pd.DataFrame(columns=column_names)
 
 
 def safe_compare(a, b, operator):
     def compare(x, y):
         # Allow int and float to compare with each other
-        if not (
-            isinstance(x, (int, float)) and isinstance(y, (int, float))
-        ) and type(x) != type(y):
+        if not (isinstance(x, (int, float)) and isinstance(y, (int, float))) and type(
+            x
+        ) != type(y):
             return False
 
         if operator == "==":
@@ -53,13 +57,10 @@ def safe_compare(a, b, operator):
         return [compare(item, b) for item in a]
     else:
         return compare(a, b)
-    
 
-def append_meta(
-    metadata: pd.DataFrame,
-    dataset: str
-) -> pd.DataFrame:
-    '''
+
+def append_meta(metadata: pd.DataFrame, dataset: str) -> pd.DataFrame:
+    """
     Extract data from yaml file and append it to the metadata.
 
     Args:
@@ -68,53 +69,129 @@ def append_meta(
 
     Returns:
         Metadata dataframe with extracted data appended.
-    '''
+    """
     data = sh.load_dataset(dataset)
 
-    #create measurements dataframe for the new dataset
-    measurements = pd.DataFrame(columns=["participant_ID","analyte","time","value"])
+    # create measurements dataframe for the new dataset
+    measurements = pd.DataFrame(columns=["participant_ID", "analyte", "time", "value"])
     counter = 1
     for participant in data["participants"]:
         new_data = pd.DataFrame.from_dict(participant["measurements"])
         new_data["participant_ID"] = counter
         if len(measurements) == 0:
-            measurements = new_data[["participant_ID","analyte","time","value"]]
+            measurements = new_data[["participant_ID", "analyte", "time", "value"]]
         else:
-            measurements = pd.concat([measurements, new_data], axis=0, join='inner', ignore_index=True)
+            measurements = pd.concat(
+                [measurements, new_data], axis=0, join="inner", ignore_index=True
+            )
         counter += 1
 
     for analyte in data["analytes"]:
         measurements_filtered = measurements.loc[measurements["analyte"] == analyte]
-        #calculate summary statistics
+        # calculate summary statistics
         n_samples = len(measurements_filtered)
         n_unique_participants = len(set(measurements_filtered["participant_ID"]))
-        n_negative = len(measurements_filtered.loc[
-                         [(value == "negative") | 
-                          ((data["analytes"][analyte]["unit"] != "cycle threshold") & 
-                           (safe_compare(value, data["analytes"][analyte]["limit_of_detection"], "<"))) | 
-                          ((data["analytes"][analyte]["unit"] == "cycle threshold") & 
-                           (safe_compare(value, data["analytes"][analyte]["limit_of_detection"], ">")))
-                         for value in measurements_filtered["value"]]
-                        ])
-        n_positive = len(measurements_filtered.loc[
-                         [(value == "positive") | 
-                          ((data["analytes"][analyte]["unit"] != "cycle threshold") &  
-                           (safe_compare(value, data["analytes"][analyte]["limit_of_quantification"], "<"))) | 
-                          ((data["analytes"][analyte]["unit"] == "cycle threshold") & 
-                           (safe_compare(value, data["analytes"][analyte]["limit_of_detection"], "<") | 
-                            ((data["analytes"][analyte]["limit_of_detection"] == "unknown") & (safe_compare(value, 0, ">")))))
-                         for value in measurements_filtered["value"]]
-                        ])
-        n_quantified = len(measurements_filtered.loc[
-                           [((data["analytes"][analyte]["unit"] != "cycle threshold") & 
-                             (data["analytes"][analyte]["limit_of_quantification"] == "unknown") & 
-                             (safe_compare(value, 0, ">"))) | 
-                            ((data["analytes"][analyte]["unit"] != "cycle threshold") & 
-                             (data["analytes"][analyte]["limit_of_quantification"] != "unknown") & 
-                             (safe_compare(value, data["analytes"][analyte]["limit_of_quantification"], ">")))  
-                         for value in measurements_filtered["value"]]  
-                        ])
-        new_line = [dataset] + [data["analytes"][analyte][key] for key in metadata.columns if key in list(data["analytes"][analyte].keys())] + [n_samples, n_unique_participants, n_negative, n_positive, n_quantified]
+        n_negative = len(
+            measurements_filtered.loc[
+                [
+                    (value == "negative")
+                    | (
+                        (data["analytes"][analyte]["unit"] != "cycle threshold")
+                        & (
+                            safe_compare(
+                                value,
+                                data["analytes"][analyte]["limit_of_detection"],
+                                "<",
+                            )
+                        )
+                    )
+                    | (
+                        (data["analytes"][analyte]["unit"] == "cycle threshold")
+                        & (
+                            safe_compare(
+                                value,
+                                data["analytes"][analyte]["limit_of_detection"],
+                                ">",
+                            )
+                        )
+                    )
+                    for value in measurements_filtered["value"]
+                ]
+            ]
+        )
+        n_positive = len(
+            measurements_filtered.loc[
+                [
+                    (value == "positive")
+                    | (
+                        (data["analytes"][analyte]["unit"] != "cycle threshold")
+                        & (
+                            safe_compare(
+                                value,
+                                data["analytes"][analyte]["limit_of_quantification"],
+                                "<",
+                            )
+                        )
+                    )
+                    | (
+                        (data["analytes"][analyte]["unit"] == "cycle threshold")
+                        & (
+                            safe_compare(
+                                value,
+                                data["analytes"][analyte]["limit_of_detection"],
+                                "<",
+                            )
+                            | (
+                                (
+                                    data["analytes"][analyte]["limit_of_detection"]
+                                    == "unknown"
+                                )
+                                & (safe_compare(value, 0, ">"))
+                            )
+                        )
+                    )
+                    for value in measurements_filtered["value"]
+                ]
+            ]
+        )
+        n_quantified = len(
+            measurements_filtered.loc[
+                [
+                    (
+                        (data["analytes"][analyte]["unit"] != "cycle threshold")
+                        & (
+                            data["analytes"][analyte]["limit_of_quantification"]
+                            == "unknown"
+                        )
+                        & (safe_compare(value, 0, ">"))
+                    )
+                    | (
+                        (data["analytes"][analyte]["unit"] != "cycle threshold")
+                        & (
+                            data["analytes"][analyte]["limit_of_quantification"]
+                            != "unknown"
+                        )
+                        & (
+                            safe_compare(
+                                value,
+                                data["analytes"][analyte]["limit_of_quantification"],
+                                ">",
+                            )
+                        )
+                    )
+                    for value in measurements_filtered["value"]
+                ]
+            ]
+        )
+        new_line = (
+            [dataset]
+            + [
+                data["analytes"][analyte][key]
+                for key in metadata.columns
+                if key in list(data["analytes"][analyte].keys())
+            ]
+            + [n_samples, n_unique_participants, n_negative, n_positive, n_quantified]
+        )
         metadata.loc[len(metadata)] = new_line
     return metadata
 
@@ -131,12 +208,14 @@ def __main__() -> None:
         metadata = append_meta(metadata, filename)
 
     # Remove "\n" from description
-    metadata["description"] = [item.replace("\n", "") for item in metadata["description"]]
+    metadata["description"] = [
+        item.replace("\n", "") for item in metadata["description"]
+    ]
 
     # Convert to dict, then dump to YAML
-    with open('data/metadata.yaml', 'w') as f:
+    with open("data/metadata.yaml", "w") as f:
         yaml.safe_dump(metadata.to_dict(orient="records"), f)
-        
+
 
 if __name__ == "__main__":
     __main__()
