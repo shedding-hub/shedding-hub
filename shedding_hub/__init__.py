647573a24692d3f4aab477896b08a0feb0e5ec9a
Yuke Wang <yuke.wang@emory.edu>
2025-08-18 23:47:43 -0500

    Rebase the shedding peak PR with the main, which was merged with PR 131.

--- shedding_hub/__init__.py
+++ shedding_hub/__init__.py
@@ -5,6 +5,15 @@ from .shedding_duration import (
     plot_shedding_duration,
     plot_shedding_durations,
 )
+<<<<<<< HEAD
+=======
+from .shedding_peak import (
+    calc_shedding_peak,
+    calc_shedding_peaks,
+    plot_shedding_peak,
+    plot_shedding_peaks,
+)
+>>>>>>> 383ad57 (Harmonize shedding_duration and shedding_peak)
 
 __all__ = [
     "folded_str",
@@ -15,4 +24,11 @@ __all__ = [
     "calc_shedding_durations",
     "plot_shedding_duration",
     "plot_shedding_durations",
+<<<<<<< HEAD
+=======
+    "calc_shedding_peak",
+    "calc_shedding_peaks",
+    "plot_shedding_peak",
+    "plot_shedding_peaks",
+>>>>>>> 383ad57 (Harmonize shedding_duration and shedding_peak)
 ]
